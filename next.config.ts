8f94412385861992a6308bd9d649b58b3e01ff10
has815 <bhattihaseeb008@gmail.com>
2025-12-14 15:55:28 +0500

    Enable standalone output in Next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-}
-
+ /** @type {import('next').NextConfig} */ 
+ const nextConfig = {
+     output: 'standalone', }
 module.exports = nextConfig
